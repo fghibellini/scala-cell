730feeee0cca2eb381cdd3c0327017de3a192359
Filippo Ghibellini <filippo.ghibellini@zalando.de>
2021-05-07 22:49:44 +0200

    done

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -10,12 +10,23 @@ object SubdivisionDirection {
   final case object Vertical extends SubdivisionDirection
 }
 
-// TODO can the root be a LeafCell ?
-sealed abstract class Cell[A] {
-  def f[M[_]: Monad, B](fn : A => M[B]) : M[Cell[B]]
-}
+// From the assignment it would sound that the root should not directly be a value
+// but that would just introduce a lot of boilerplate code (we would need a second class)
+// so I'll have a single type to represent nodes of the tree.
+
+sealed abstract class Cell[A]
+final case class DivisionCell[A](xs: List[Cell[A]], direction: SubdivisionDirection) extends Cell[A]
+final case class LeafCell[A](value: A) extends Cell[A]
 
 object Cell {
+
+  def f[M[_]: Monad, A, B](fn : A => M[B])(d: Cell[A]) : M[Cell[B]] = d match {
+    case DivisionCell(xs, direction) =>
+      xs.traverse(x => f(fn)(x)).map(xs_ => DivisionCell(xs_, direction))
+    case LeafCell(value) =>
+      fn(value).map(x => LeafCell(x))
+  }
+
   implicit def showCell[A : Show]: Show[Cell[A]] = Show.show(cell => cell match {
     case DivisionCell(xs, direction) => {
       val contents = Foldable[List].intercalate(xs.map(x => x.show), ",")
@@ -24,16 +35,7 @@ object Cell {
     }
     case LeafCell(value) => value.toString()
   })
-}
 
-case class DivisionCell[A](xs: List[Cell[A]], direction: SubdivisionDirection) extends Cell[A] {
-  def f[M[_]: Monad, B](fn : A => M[B]) : M[Cell[B]] =
-    xs.traverse(x => x.f(fn)).map(xs_ => DivisionCell(xs_, direction))
-}
-
-case class LeafCell[A](value: A) extends Cell[A] {
-  def f[M[_]: Monad, B](fn : A => M[B]) : M[Cell[B]] =
-    fn(value).map(x => LeafCell(x))
 }
 
 object Hello extends App {
--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -11,45 +11,41 @@ import cats.implicits._
 
 object DSpecification extends Properties("D.f") {
 
-  def cellArbitrary[A : Arbitrary](depth : Int) : Arbitrary[Cell[A]] = Arbitrary(if (depth < 2) {
-      for {
-        value <- Arbitrary.arbitrary[A]
-      } yield LeafCell(value)
-   } else {
-     for {
-      n <- Gen.posNum[Int]
-      x <- n.abs % 3 match {
-        case 0 => {
-          implicit def subcellArbitrary[A : Arbitrary] : Arbitrary[Cell[A]] = cellArbitrary(depth - 1)
-          for {
-            subcells <- Arbitrary.arbitrary[List[Cell[A]]]
-          } yield DivisionCell(subcells, SubdivisionDirection.Vertical)
-        }
-        case 1 => {
-          implicit def subcellArbitrary[A : Arbitrary] : Arbitrary[Cell[A]] = cellArbitrary(depth - 1)
-          for {
-            subcells <- Arbitrary.arbitrary[List[Cell[A]]]
-          } yield DivisionCell(subcells, SubdivisionDirection.Horizontal)
-        }
-        case 2 => {
-          for {
-            value <- Arbitrary.arbitrary[A]
-          } yield LeafCell(value)
-        }
-      }
-    } yield x
-  })
+  def arbitraryLeafCell[A : Arbitrary] = for {
+      value <- Arbitrary.arbitrary[A](implicitly[Arbitrary[A]])
+    } yield LeafCell(value)
 
-  implicit val cellArbitrary: Arbitrary[Cell[Int]] = cellArbitrary(4)
+  def arbitraryDivisionCell[A : Arbitrary](direction : SubdivisionDirection, maxDepth : Int) =
+      {
+        implicit def subcellArbitrary[A : Arbitrary] : Arbitrary[Cell[A]] = cellArbitraryInst(maxDepth)
+        for {
+          subcells <- Arbitrary.arbitrary[List[Cell[A]]]
+        } yield DivisionCell(subcells, direction)
+      }
+
+  def cellArbitraryInst[A : Arbitrary](maxDepth : Int) : Arbitrary[Cell[A]] = Arbitrary(
+    if (maxDepth < 2) {
+      arbitraryLeafCell(implicitly[Arbitrary[A]])
+    } else {
+      for {
+        n <- Gen.posNum[Int]
+        x <- n.abs % 3 match {
+          case 0 => arbitraryDivisionCell(SubdivisionDirection.Vertical, maxDepth - 1)(implicitly[Arbitrary[A]])
+          case 1 => arbitraryDivisionCell(SubdivisionDirection.Horizontal, maxDepth - 1)(implicitly[Arbitrary[A]])
+          case 2 => arbitraryLeafCell(implicitly[Arbitrary[A]])
+        }
+      } yield x
+    }
+  )
+
+  implicit val cellArbitrary: Arbitrary[Cell[Int]] = cellArbitraryInst(4)
 
   property("id") = forAll { (a: Cell[Int]) =>
-    implicit val cellShow : Show[Cell[Int]] = Cell.showCell
-    println(a.show)
-    a.f[Id, Int](identity) == a
+    Cell.f[Id, Int, Int](identity)(a) == a
   }
 
   property("some") = forAll { (a: Cell[Int]) =>
-    a.f[Option, Int](Some(_)) == Some(a)
+    Cell.f[Option, Int, Int](Some(_))(a) == Some(a)
   }
 
 }
